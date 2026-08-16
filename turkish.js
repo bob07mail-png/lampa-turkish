f9661c2a8febf15c3aa759f45ff2292e6c2ce898
bob07mail-png <bob07mail@gmail.com>
2026-08-16 12:27:39 +0300

    Update turkish.js

--- turkish.js
+++ turkish.js
@@ -1,27 +1,57 @@
 (function () {
     'use strict';
 
-    function addTurkishMenu() {
-        Lampa.Menu.add({
-            title: 'Турецькі серіали',
-            icon: '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>',
-            click: function () {
+    function addTurkishSection() {
+        // Реєструємо компонент турецьких серіалів
+        Lampa.Component.add('turkish_series', function () {
+            this.create = function () {
+                var _this = this;
+                this.activity.loader(true);
+
+                Lampa.Api.sources.tmdb.get('discover/tv?with_origin_country=TR&sort_by=popularity.desc', {}, function (data) {
+                    _this.build(data);
+                }, function () {
+                    _this.empty();
+                });
+            };
+
+            this.build = function (data) {
+                var _this = this;
+                this.activity.loader(false);
+                
                 Lampa.Activity.push({
                     title: 'Турецькі серіали',
-                    url: 'discover/tv?with_origin_country=TR&sort_by=popularity.desc',
                     component: 'category_full',
                     source: 'tmdb',
-                    page: 1
+                    card_type: 'tv',
+                    results: data.results,
+                    more: data
                 });
+            };
+        });
+
+        // Додаємо пункт у головне меню
+        var menu_item = {
+            title: 'Турецькі серіали',
+            icon: '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>',
+            component: 'turkish_series'
+        };
+
+        // Вставляємо в ліве меню
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') {
+                if (Lampa.Menu.add) {
+                    Lampa.Menu.add(menu_item);
+                }
             }
         });
     }
 
     if (window.appready) {
-        addTurkishMenu();
+        addTurkishSection();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') addTurkishMenu();
+            if (e.type === 'ready') addTurkishSection();
         });
     }
 })();
